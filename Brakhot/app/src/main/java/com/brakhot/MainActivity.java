042ee8f7c1b67d7944ff5669060eef755411f63d
sergey-korchagin <sergey.korchagin.il@gmail.com>
2016-05-15 23:05:53 +0300

    toolbar added

---
+++ Brakhot/app/src/main/java/com/brakhot/MainActivity.java
@@ -0,0 +1,53 @@
+package com.brakhot;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle(R.string.app_name);
+        mToolbar.setBackgroundColor(Color.CYAN);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+
+        switch (id) {
+//            case R.id.action_settings: {
+//                return true;
+//            }
+            case R.id.action_favorite: {
+                return true;
+            }
+            default: {
+                return super.onOptionsItemSelected(item);
+
+            }
+        }
+
+    }
+}
---
+++ Brakhot/app/src/main/java/com/brakhot/Utils/FlyModeUtils.java
@@ -0,0 +1,7 @@
+package com.brakhot.Utils;
+
+/**
+ * Created by serge_000 on 15/05/2016.
+ */
+public class FlyModeUtils {
+}
