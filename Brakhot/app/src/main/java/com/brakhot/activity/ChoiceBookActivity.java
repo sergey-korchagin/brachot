ee3e4e7501053b954e5d6c57e92d189f2499eaf6
sergey-korchagin <sergey.korchagin.il@gmail.com>
2016-06-06 23:20:18 +0300

    ads animation

--- Brakhot/app/src/main/java/com/brakhot/activity/ChoiceBookActivity.java
+++ Brakhot/app/src/main/java/com/brakhot/activity/ChoiceBookActivity.java
@@ -3,35 +3,27 @@ package com.brakhot.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.media.session.MediaSessionCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.brakhot.R;
 import com.brakhot.adapter.AdsViewPagerAdapter;
 import com.brakhot.adapter.BookmarksGridAdapter;
-import com.brakhot.adapter.ViewPagerAdapter;
-import com.brakhot.manager.AdsDataManager;
-import com.squareup.picasso.Picasso;
-import com.viewpagerindicator.CirclePageIndicator;
 import com.viewpagerindicator.LinePageIndicator;
-import com.viewpagerindicator.TitlePageIndicator;
 
 public class ChoiceBookActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
-
+    private static final String TAG = ChoiceBookActivity.class.getSimpleName();
     private GridView mGridView;
     private BookmarksGridAdapter mBookmarksGridAdapter;
 
     private ViewPager mPager;
     private AdsViewPagerAdapter mAdapter;
-
+    private int mViewPagerCounter = 0;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,7 +42,29 @@ public class ChoiceBookActivity extends AppCompatActivity implements AdapterView
         LinePageIndicator titleIndicator = (LinePageIndicator) findViewById(R.id.titles);
         titleIndicator.setLineWidth(30);
         titleIndicator.setViewPager(mPager);
+        autoPlay(mPager);
+    }
 
+
+
+    private void autoPlay(final ViewPager viewPager) {
+
+        viewPager.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    if (mAdapter != null && viewPager.getAdapter().getCount() > 0) {
+                        int position = mViewPagerCounter % mAdapter.getCount();
+                        mViewPagerCounter++;
+                        viewPager.setCurrentItem(position);
+                        autoPlay(viewPager);
+
+                    }
+                } catch (Exception e) {
+                    Log.e(TAG, "auto scroll pager error.", e);
+                }
+            }
+        }, 5000);
     }
 
     @Override
