9e4532bb710a528f93d1b8527b63a43f232f5b28
sergey-korchagin <sergey.korchagin.il@gmail.com>
2016-06-07 21:17:08 +0300

    delete grid item

--- Brakhot/app/src/main/java/com/brakhot/adapter/BookmarksGridAdapter.java
+++ Brakhot/app/src/main/java/com/brakhot/adapter/BookmarksGridAdapter.java
@@ -16,6 +16,7 @@ import java.util.List;
 public class BookmarksGridAdapter extends BaseAdapter {
     private Context context;
     private List<String> mGridItemsList = new ArrayList<>();
+    private BookmarksGridAdapter mBookmarksGridAdapter = this;
 
     public BookmarksGridAdapter(Context context) {
         this.context = context;
@@ -24,25 +25,44 @@ public class BookmarksGridAdapter extends BaseAdapter {
         }
     }
 
-    public View getView(int position, View convertView, ViewGroup parent) {
 
-        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View gridView;
-        if (convertView == null) {
-            gridView = inflater.inflate(R.layout.bookmarks_grid_item, null);
-            TextView textView = (TextView) gridView
-                    .findViewById(R.id.textV);
-            textView.setText(mGridItemsList.get(position));
-//            ImageView imageView = (ImageView) gridView
-//                    .findViewById(R.id.grid_item_image);
+    public static class ViewHolder
+    {
+        TextView textView;
+        ImageView close;
+    }
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent)
+    {
+        ViewHolder holder;
+        if(convertView==null)
+        {
+            convertView = LayoutInflater.from(context).inflate(R.layout.bookmarks_grid_item,null);
 
-
-        } else {
-            gridView = convertView;
+            holder = new ViewHolder();
+            holder.textView = (TextView) convertView.findViewById(R.id.textV);
+            holder.close = (ImageView) convertView.findViewById(R.id.buttonDelete);
+            convertView.setTag(holder);
         }
 
-        return gridView;
+        else{
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        holder.textView.setText(mGridItemsList.get(position));
+        holder.close.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View arg1)
+            {
+                mGridItemsList.remove(position);
+                mBookmarksGridAdapter.notifyDataSetChanged();
+            }
+        });
+        return convertView;
+
     }
+
+
 
     @Override
     public int getCount() {
